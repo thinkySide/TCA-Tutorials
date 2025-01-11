9baf5566d5240396ea6750c04e0d4bd64491c41b
thinkyside <eunlyuing@gmail.com>
2025-01-11 12:54:02 +0900

    Essentials - Adding side effects

--- TCA-Tutorials/TCA-Tutorials/CounterFeature.swift
+++ TCA-Tutorials/TCA-Tutorials/CounterFeature.swift
@@ -6,6 +6,7 @@
 //
 
 import ComposableArchitecture
+import Foundation
 
 /// Reducer 매크로? : Reducer 프로토콜에 맞게 타입을 확장해줌.
 @Reducer
@@ -18,6 +19,9 @@ struct CounterFeature {
     @ObservableState
     struct State {
         var count = 0
+        var fact: String?
+        var isLoading = false
+        var isTimerRunning = false
     }
     
     /// Feature에서 수행할 모든 작업을 보관하는 Action
@@ -25,7 +29,16 @@ struct CounterFeature {
     /// UI에서 수행하는 작업 그대로 나타내는 것을 권장함.
     enum Action {
         case decrementButtonTapped
+        case factButtonTapped
+        case factResponse(String)
         case incrementButtonTapped
+        case timerTick
+        case toggleTimerButtonTapped
+    }
+    
+    /// Effect 취소
+    enum CancelID {
+        case timer
     }
     
     /// Reducer를 준수하려면 body 계산 속성을 꼭 구현해줘야 한다.
@@ -39,11 +52,65 @@ struct CounterFeature {
                 /// 이 경우에는 아무것도 실행할 필요가 없기 때문에 .none 반환
             case .decrementButtonTapped:
                 state.count -= 1
+                state.fact = nil
+                return .none
+                
+            case .factButtonTapped:
+                state.fact = nil
+                state.isLoading = true
+                
+                /// 사이드 이펙트를 실행하는데 적합한 TCA의 도구, Effect
+                ///
+                /// Effect를 구성하는 기본 방법은 run 타입 함수를 사용하는 것.
+                /// 이를 통해 원하는 모든 종류의 작업을 수행할 수 있는 비동기 컨텍스트와 작업을
+                /// 시스템으로 다시 보내기 위한 핸들(send)를 제공합니다.
+                return .run { [count = state.count] send in
+                    let (data, _) = try await URLSession.shared
+                        .data(from: URL(string: "http://numbersapi.com/\(count)")!)
+                    let fact = String(decoding: data, as: UTF8.self)
+                    
+                    /// 이렇게 직접 내부 속성을 업데이트 할 수 없다.
+                    /// sendable 클로저가 inout state를 캡처할 수 없기 때문에
+                    /// 컴파일러에서 엄격하게 적용하는 것.
+                    /// 이는 순수한 State의 변형과 지저분하고 복잡한 Effect를 분리하는 방법을 보여줌.
+                    // state.fact = fact
+                    
+                    /// 이렇게 새로운 Action을 fact를 담아 보내줘야 함.
+                    await send(.factResponse(fact))
+                }
+                
+                /// Effect에서 Reducer로 정보를 다시 공급하기 위한 Action
+            case let .factResponse(fact):
+                state.fact = fact
+                state.isLoading = false
                 return .none
                 
             case .incrementButtonTapped:
                 state.count += 1
+                state.fact = nil
                 return .none
+                
+            case .timerTick:
+                state.count += 1
+                state.fact = nil
+                return .none
+                
+            case .toggleTimerButtonTapped:
+                state.isTimerRunning.toggle()
+                if state.isTimerRunning {
+                    return .run { send in
+                        /// 1초마다 타이머 틱 작동
+                        while true {
+                            try await Task.sleep(for: .seconds(1))
+                            await send(.timerTick)
+                        }
+                    }
+                    /// Effect를 취소 가능하게 표시하게 해주는 것.
+                    .cancellable(id: CancelID.timer)
+                } else {
+                    /// Effect를 취소 하는 방법
+                    return .cancel(id: CancelID.timer)
+                }
             }
         }
     }
--- TCA-Tutorials/TCA-Tutorials/CounterView.swift
+++ TCA-Tutorials/TCA-Tutorials/CounterView.swift
@@ -45,6 +45,30 @@ struct CounterView: View {
                 .background(Color.black.opacity(0.1))
                 .cornerRadius(10)
             }
+            Button(store.isTimerRunning ? "Stop timer" : "Start timer") {
+                store.send(.toggleTimerButtonTapped)
+            }
+            .font(.largeTitle)
+            .padding()
+            .background(Color.black.opacity(0.1))
+            .cornerRadius(10)
+            
+            Button("Fact") {
+                store.send(.factButtonTapped)
+            }
+            .font(.largeTitle)
+            .padding()
+            .background(Color.black.opacity(0.1))
+            .cornerRadius(10)
+            
+            if store.isLoading {
+                ProgressView()
+            } else if let fact = store.fact {
+                Text(fact)
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+                    .padding()
+            }
         }
     }
 }
